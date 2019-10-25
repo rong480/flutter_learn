430382490c46b64589f3a6ca240227f29fe43ce6
rongjiaqi <375107109@qq.com>
2019-10-25 15:13:05 +0800

    base document add code

---
+++ app/demoDart/day_01/helloworld.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(
+    new Center(
+      child: new Text(
+        "hello flutter",
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
